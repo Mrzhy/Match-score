9027652c34634bab0a089fe0553c32da6f0c75ed
Mr.Zhao <mister.zhao@foxmail.com>
2016-04-25 21:23:54 +0800

    增加加减分功能
    增加加减分功能

--- Match score/ViewController.swift
+++ Match score/ViewController.swift
@@ -10,6 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var screen1: UITextField!
+    
+    @IBOutlet weak var screen2: UITextField!
+    
+    var a=0
+    var b=0
+    
+    @IBAction func screen1add(sender: UIButton) {
+        a=a+1
+        screen1.text=("\(a)")
+            }
+    
+    @IBAction func screen1subtract(sender: UIButton) {
+        if a>0{
+            a=a-1
+            screen1.text=("\(a)")
+            return
+        }
+        screen1.text=("\(a)")
+    }
+    
+    @IBAction func screen2add(sender: UIButton) {
+        b=b+1
+        screen2.text=("\(b)")
+    }
+    
+    @IBAction func screen2subtract(sender: UIButton) {
+        if b>0{
+            b=b-1
+            screen2.text=("\(b)")
+            return
+        }
+        screen1.text=("\(b)")
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
