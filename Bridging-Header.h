28dc690be0bb06c897024ac3bdb65730d4d58ebf
Mr.Zhao <mister.zhao@foxmail.com>
2016-05-05 12:10:59 +0800

    连接到数据库
    队名和比分可保存到数据库

---
+++ Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  Match score
+//
+//  Created by Mr.Zhao on 16/5/5.
+//  Copyright © 2016年 Mr.Zhao. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+
+#endif /* Bridging_Header_h */
+#import "sqlite3.h"
+#import <time.h>
--- Match score/ViewController.swift
+++ Match score/ViewController.swift
@@ -9,61 +9,121 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var screen1: UITextField!
     
-    @IBOutlet weak var screen2: UITextField!
+    var db : SQLiteDB!
+
+    @IBOutlet weak var team1: UITextField!
+    
+    @IBOutlet weak var team2: UITextField!
+    
+    @IBOutlet weak var score1: UITextField!
+    
+    @IBOutlet weak var score2: UITextField!
     
     var a=0
     var b=0
     
     @IBAction func screen1add(sender: UIButton) {
         a=a+1
-        screen1.text=("\(a)")
+        score1.text=("\(a)")
             }
     
     @IBAction func screen1subtract(sender: UIButton) {
         if a>0{
             a=a-1
-            screen1.text=("\(a)")
+            score1.text=("\(a)")
             return
         }
-        screen1.text=("\(a)")
+        score1.text=("\(a)")
     }
     
     @IBAction func screen2add(sender: UIButton) {
         b=b+1
-        screen2.text=("\(b)")
+        score2.text=("\(b)")
     }
     
     @IBAction func screen2subtract(sender: UIButton) {
         if b>0{
             b=b-1
-            screen2.text=("\(b)")
+            score2.text=("\(b)")
             return
         }
-        screen1.text=("\(b)")
+        score1.text=("\(b)")
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //获取数据库实例
+        db = SQLiteDB.sharedInstance()
+        //如果表还不存在则创建表（其中uid为自增主键）
+        db.execute("create table if not exists t_user(uid integer primary key,team1 varchar(20),team2 varchar(20))")
+        //如果有数据则加载
+        initUser()
+        
+        //获取数据库实例
+        db = SQLiteDB.sharedInstance()
+        //如果表还不存在则创建表（其中uid为自增主键）
+        db.execute("create table if not exists t_mark(uid integer primary key,score1 varchar(20),score2 varchar(20))")
+        //如果有数据则加载
+        initMark()
+
+    }
+    
+    //点击保存
+    @IBAction func saveClicked(sender: AnyObject) {
+        saveUser()
+        saveMark()
+    }
+    
+    //从SQLite加载数据
+    func initUser() {
+        let data = db.query("select * from t_user")
+            if data.count > 0 {
+            //获取最后一行数据显示
+            let user = data[data.count - 1]
+            team1.text = user["team1"] as? String
+            team2.text = user["team2"] as? String
+        }
+    }
+    
+    func initMark() {
+        let data = db.query("select * from t_mark")
+        if data.count > 0 {
+            //获取最后一行数据显示
+            let mark = data[data.count - 1]
+            score1.text = mark["score1"] as? String
+            score2.text = mark["score2"] as? String
+        }
     }
 
+    
+    //保存数据到SQLite
+    func saveUser() {
+        let team1 = self.team1.text!
+        let team2 = self.team2.text!
+        //插入数据库，这里用到了esc字符编码函数，其实是调用bridge.m实现的
+        let sql = "insert into t_user(team1,team2) values('\(team1)','\(team2)')"
+        print("sql: \(sql)")
+        //通过封装的方法执行sql
+        let result = db.execute(sql)
+        print(result)
+    }
+    
+    func saveMark() {
+        let score1 = self.score1.text!
+        let score2 = self.score2.text!
+        //插入数据库，这里用到了esc字符编码函数，其实是调用bridge.m实现的
+        let sql1 = "insert into t_mark(score1,score2) values('\(score1)','\(score2)')"
+        print("sql1: \(sql1)")
+        //通过封装的方法执行sql
+        let result1 = db.execute(sql1)
+        print(result1)
+    }
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
